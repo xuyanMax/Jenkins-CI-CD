2e43df29eb2f517b094b4cd34442efb529e6f4e2
Xu Yan <418780414@qq.com>
2019-01-29 10:45:21 +0800

    add two modules

---
+++ hongxing-nange/src/test/java/com/hongxing/nangeTest.java
@@ -0,0 +1,11 @@
+package com.hongxing;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class nangeTest {
+    @Test
+    public void test() {
+        Assert.assertNotEquals("b", "918");
+    }
+}
---
+++ hongxing-shaji/src/test/java/com/hongxing/shajiTest.java
@@ -0,0 +1,11 @@
+package com.hongxing;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class shajiTest {
+    @Test
+    public void test() {
+        Assert.assertNotEquals("a", "b");
+    }
+}
