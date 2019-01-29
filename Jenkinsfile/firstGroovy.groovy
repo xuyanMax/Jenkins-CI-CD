eacd6e0a97817bbe5bdc87834c3b0847c9180ebd
Xu Yan <418780414@qq.com>
2019-01-29 11:15:33 +0800

    fix duplicate dependencies

--- Jenkinsfile/firstGroovy.groovy
+++ Jenkinsfile/firstGroovy.groovy
@@ -14,13 +14,13 @@ pipeline {
         }
         stage('test') {
             steps {
-                sh 'mvn test'
+                sh 'mvn clean test'
             }
         }
 
         stage('install') {
             steps {
-                sh 'mvn install'
+                sh 'mvn clean install'
             }
         }
     }
